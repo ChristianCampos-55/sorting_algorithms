9680654788a1aa45cfd3cb6cf98a7555bd07c926
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-17 19:38:55 +0000

    [ADD] Complying with betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include "sort.h"
+
+/**
+ * bubble_sort - Function that executes a Bubble sort on an array
+ * @array: array to sort
+ * @size: size of array to sort
+ *
+ * Return: Always 0
+ */
+
 void bubble_sort(int *array, size_t size)
 {
 
 	unsigned int a, b, c;
-	for (a = 0; a < size-1; a++)
 
-		for (b = 0; b < size-1; b++)
-			if (array[b] > array[b+1])
+	for (a = 0; a < size - 1; a++)
+		for (b = 0; b < size - 1; b++)
+			if (array[b] > array[b + 1])
 			{
 				c = array[b];
 				array[b] = array[b + 1];
